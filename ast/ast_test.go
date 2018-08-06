cd7addfb12dd9f6ff53313a3948edd7f7674275e
Xu Jiajun <xujiajun@yeecall.com>
2018-08-06 17:36:18 +0800

    parser

---
+++ ast/ast_test.go
@@ -0,0 +1,49 @@
+/*
+ * // Copyright (C) 2017 gyee authors
+ * //
+ * // This file is part of the gyee library.
+ * //
+ * // the gyee library is free software: you can redistribute it and/or modify
+ * // it under the terms of the GNU General Public License as published by
+ * // the Free Software Foundation, either version 3 of the License, or
+ * // (at your option) any later version.
+ * //
+ * // the gyee library is distributed in the hope that it will be useful,
+ * // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * // GNU General Public License for more details.
+ * //
+ * // You should have received a copy of the GNU General Public License
+ * // along with the gyee library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package ast
+
+import (
+	"testing"
+
+	"github.com/yeeco/yvm/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
---
+++ parser/parser_test.go
@@ -0,0 +1,119 @@
+/*
+ * // Copyright (C) 2017 gyee authors
+ * //
+ * // This file is part of the gyee library.
+ * //
+ * // the gyee library is free software: you can redistribute it and/or modify
+ * // it under the terms of the GNU General Public License as published by
+ * // the Free Software Foundation, either version 3 of the License, or
+ * // (at your option) any later version.
+ * //
+ * // the gyee library is distributed in the hope that it will be useful,
+ * // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * // GNU General Public License for more details.
+ * //
+ * // You should have received a copy of the GNU General Public License
+ * // along with the gyee library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package parser
+
+import (
+	"testing"
+
+	"github.com/yeeco/yvm/ast"
+	"github.com/yeeco/yvm/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+   let x = 5;
+   let y = 10;
+   let foobar = 838383;
+   `
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+	return true
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+   return 5;
+   return 10;
+   return 993322;
+   `
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+		}
+	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
